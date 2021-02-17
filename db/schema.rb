462c30ec0f8690c5f081ee904deac6031921c63c
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-17 15:51:34 -0300

    review model created with dependency to restaurants and respective db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_181537) do
+ActiveRecord::Schema.define(version: 2021_02_17_184920) do
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name", null: false
     t.text "address", null: false
@@ -19,4 +20,14 @@ ActiveRecord::Schema.define(version: 2021_02_17_181537) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
